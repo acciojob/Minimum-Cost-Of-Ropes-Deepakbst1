fde29661b757b89d054aa89338f030aea0c55c3d
Deepak Kumar Deepak kumar <91386273+Deepakbst1@users.noreply.github.com>
2023-05-16 22:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  // Get the input value and convert it to an array of integers
+  const input = document.getElementById('ropeLengths').value;
+  const lengths = input.split(',').map((length) => parseInt(length.trim(), 10));
+
+  // Calculate the minimum cost
+  const minCost = calculateMinimumCost(lengths);
+
+  // Update the result div with the minimum cost
+  resultDiv.textContent = `Minimum Cost: ${minCost}`;
+}
